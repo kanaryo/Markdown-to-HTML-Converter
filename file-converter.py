2a96c98557e31b08e8201edffa4f7455d4e1f835
kanaryo <kanaryo.wa.water7@gmail.com>
2025-05-05 18:41:16 +0900

    マークダウンからHTMLい変換する処理の実装

---
+++ file-converter.py
@@ -0,0 +1,28 @@
+import sys
+import markdown
+
+def validate_args(expected_args):
+    if len(sys.argv) < expected_args:
+        print(f"Error: Expected at least {expected_args} arguments, but got {len(sys.argv)}.")
+        sys.exit(1)
+
+# コマンドとファイルパスの取得（最低でも2つの引数が必要）
+if len(sys.argv) < 3:
+    print("Usage:")
+    print(" file-converter.py markdown <inputfile> <outputfile>")
+    sys.exit(1)
+        
+inputfile = sys.argv[2]
+outputfile = sys.argv[3]
+
+if sys.argv[1] == 'markdown':
+    validate_args(4)
+    with open(inputfile) as f:
+        contents = f.read()
+    f.closed
+        
+    html = markdown.markdown(contents)
+    
+    with open(outputfile, 'w') as f:
+        f.write(html)
+    f.closed
